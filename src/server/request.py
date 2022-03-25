489c3ba24ca1c8a1090e9cf020aee9b24afe9efe
Yair Etkes <yair.etkes@gmail.com>
2022-03-25 12:10:39 +0300

    working on requests

--- src/server/request.py
+++ src/server/request.py
@@ -1,7 +1,15 @@
 from asyncio.windows_events import NULL
+from calendar import c
 import socket
 import struct
 
+
+# variables needed to the use of the class
+client_id_idx = 0
+version_idx = 1
+code_idx = 2
+payload_size_idx = 3
+payload_idx = 4
 
 class Request:
     """
@@ -12,12 +20,46 @@ class Request:
         The __init__ method will recieve data packet from client.
         """
         # TODO: implemnt the method.
-        self.request = NULL
         self.connection = conn
         self.buffer_length = buffer_length
+        self.messages = []
+        self.client_users = []
 
+        buffer_recv = self.connection.recv(self.buffer_length)
+        print("message recieved: " + str(buffer_recv) + "\n\n")
+        self.packed_buffer = struct.unpack('<16cBHI255s160s', buffer_recv)
+        print("packed_buffer = " + str(self.packed_buffer))
+
+
+    # TODO: implement
+    def sign_client(self):
+        pass
+
+    # TODO: implement
+    def send_users_list():
+        pass
 
     #TODO: implement
+    def get_public_key():
+        pass
+
+    # TODO: implement
+    def extract_msgs():
+        pass
+
+    # TODO: implement
+    def send_msg():
+        pass
+
+    # TODO: implement
+    def get_sym_key():
+        pass
+
+    # TODO: implement
+    def send_sym_key():
+        pass
+
+    # TODO: implement
     def process_request(self):
         """
         suggestion - 
@@ -28,8 +70,32 @@ class Request:
         create a sub class which is the relevant request, for instance getAllConnectionsReq...
         
         """
-        buffer_recv = self.connection.recv(self.buffer_length)
-        print("message recieved: " + str(buffer_recv) + "\n\n")
-        packed_baffer = struct.unpack('<16cBHI255s160s', buffer_recv)
-        print("packed_baffer = " + str(packed_baffer))
-        # conn.sendall(d)
+        code_signing = 110
+        code_users_list = 120
+        code_public_key = 130
+        code_extract_msgs = 140
+        code_send_msg = 150
+        code_get_sym_key = 151
+        code_send_sym_key = 152
+        
+
+        if self.packed_buffer[code_idx] == code_signing:
+            self.sign_client()
+
+        if self.packed_buffer[code_idx] == code_users_list:
+            self.send_users_list()
+
+        if self.packed_buffer[code_idx] == code_public_key:
+            self.get_public_key()
+
+        if self.packed_buffer[code_idx] == code_extract_msgs:
+            self.extract_msgs()
+
+        if self.packed_buffer[code_idx] == code_send_msg:
+            self.send_msg()
+
+        if self.packed_buffer[code_idx] == code_get_sym_key:
+            self.get_sym_key()
+
+        if self.packed_buffer[code_idx] == code_send_sym_key:
+            self.send_sym_key()
