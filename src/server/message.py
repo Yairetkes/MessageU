bee4b7798dd7eaf9e65056316e20dc7513121dfc
Yair Etkes <yair.etkes@gmail.com>
2022-04-03 15:50:05 +0300

    continue with respond code

--- src/server/message.py
+++ src/server/message.py
@@ -1,10 +1,43 @@
+import random
 
+ID_BIT_LENGTH = 32
 
 class Message:
     def __init__(self, id, toClient, fromClient, type, content) -> None:
         # TODO: input validations!!
-        self.id = id
-        self.toClient = toClient
-        self.fromClient = fromClient
-        self.type = type
-        self.content = content
+        self.__id = random.getrandbits(ID_BIT_LENGTH)
+        self.__toClient = toClient
+        self.__fromClient = fromClient
+        self.__type = type
+        self.__content = content
+
+    def get_id(self):
+        return self.__id
+
+    def get_toClient(self):
+        return self.__toClient
+
+    def get_fromClient(self):
+        return self.__fromClient
+
+    def get_type(self):
+        return self.__type
+
+    def get_content(self):
+        return self.__content
+
+    
+    # def set_id(self):
+    #     return self.__id
+
+    # def set_toClient(self):
+    #     return self.__toClient
+
+    # def set_fromClient(self):
+    #     return self.__fromClient
+
+    # def set_type(self):
+    #     return self.__type
+
+    # def set_content(self):
+    #     return self.__content
--- src/server/request.py
+++ src/server/request.py
@@ -51,28 +51,32 @@ class Request:
         # print("message recieved: " + str(self.buffer_recv) + "\n\n")
 
         # preparing the request - mostly unpacking.
-        self.packed_header = struct.unpack('<16cBHI', self.buffer_recv[0:HEADER_LENGTH])
-        struct_str = "<16sBHI" + str(int.from_bytes(self.packed_header[payload_size_idx], byteorder="little")) + "s"
-        self.request_length = HEADER_LENGTH + int.from_bytes( self.packed_header[payload_size_idx], byteorder="little")
-        print("******* request_length = ", str(self.request_length))
-        self.packed_request = struct.unpack(struct_str, self.buffer_recv[0:self.request_length])
-        print("packed_request = " + str(self.packed_request))
+        self.packed_header = struct.unpack('<16sBHI', self.buffer_recv[0:HEADER_LENGTH])
+        # struct_str = "<16sBHI" + str(int.from_bytes(self.packed_header[payload_size_idx], byteorder="little")) + "s"
+        self.payload_length = HEADER_LENGTH + int.from_bytes( self.packed_header[payload_size_idx], byteorder="little")
+        self.packed_payload = bytes()
+        # print("******* request_length = ", str(self.request_length))
+        # self.packed_request = struct.unpack(struct_str, self.buffer_recv[0:self.request_length])
+        # print("packed_request = " + str(self.packed_request))
 
 
     # TODO: test
     def sign_client(self) -> int:
         # TODO add to final report - by the protocol any user can sign up as many
-        #  times as he wants with different names. 
-        payload = self.packed_request[payload_idx]
-        name, public_key = (payload.split(b'\x00'))
-        print("name = ", name)
-        print("public_key = ", public_key)
+        #  times as he wants with different names.
+        struct_str = '255s160s'
+        self.packed_payload = struct.unpack(struct_str, self.buffer_recv[HEADER_LENGTH:])
+        name, public_key = self.packed_payload
 
-        if self.packed_header[int(client_id_idx)] in [user.get_id() for user in self.client_users]:
+        # removing padded bytes
+        name = str(name.split(b'\x00')[0])
+        if name in [user.get_name() for user in self.client_users]:
             raise NameError('The name already appear in the users list (the user already signed)')
         else:
             client = cl.clientUser(name, public_key)
             self.client_users.append(client)
+
+            res = respond.Respond()
             return client.get_id()
 
     # TODO: test
@@ -109,9 +113,30 @@ class Request:
         res = respond.Respond(PUB_KEY_RES_CEDE, len(payload), payload, self.connection)
         res.send()
 
-    # TODO: implement
+    # TODO: test
     def extract_msgs(self):
-        pass
+        if not self.messages:
+            raise error("Zero messages.")
+        else:
+            values = []
+            struct_str = '<'
+            for msg in self.messages:
+                if msg.get_toClient() == int(self.packed_header[client_id_idx]):
+                    values.append(bytes(str(msg.get_fromClient()), encoding='utf8'))
+                    values.append(bytes(str(msg.get_id()), encoding='utf8'))
+                    values.append(bytes(str(msg.get_type()), encoding='utf8'))
+                    values.append(bytes(str(len(msg.get_content())), encoding='utf8'))
+                    values.append(bytes(str(msg.get_content()), encoding='utf8'))
+
+                    # TODO: fix bugs here, maybe some need to be packed as int rather than str.
+                    struct_str += '16s4s1s4s' + str(len(msg.get_content())) + 's'
+
+            if not values:
+                raise error("No messages are intended for specific user.")
+            else:
+                payload = struct.pack(struct_str, *values)
+                res = respond.Respond(  WAIT_MSGS_RES_CODE, len(payload), payload, self.connection)
+                res.send()
 
     # TODO: test
     def send_msg(self):
@@ -125,26 +150,27 @@ class Request:
         if id not in [user.get_id() for user in self.client_users]:
             raise NameError('The id does not appear in the users list.')
         else:
-            mesg = msg.Message(id, ) #TODO: continue here - create id 
-            # self.client_users.append(client)
-            # return client.get_id()
+            mesg = msg.Message(id, self.packed_header[client_id_idx], type, message) 
+            self.messages.append(mesg)
+            id = str(id)
+            msg_id = str(mesg.get_id)
 
-    # TODO: implement
-    def get_sym_key():
-        pass
+            payload = struct.pack('<16s4s', bytes(id,encoding='utf8'), bytes(msg_id,encoding='utf8'))
+            res = respond.Respond(MSG_SENT_RES_CODE, len(payload), payload, self.connection)
+            res.send()
 
-    # TODO: implement
-    def send_sym_key():
-        pass
+    # # TODO: implement
+    # def get_sym_key():
+    #     pass
+
+    # # TODO: implement
+    # def send_sym_key():
+    #     pass
 
     # TODO: implement
     def error_response(self):
         res = respond.Respond(ERROR_RES_CODE, 0, bytes(), self.connection)
         res.send()
-
-    # TODO: implement
-    def sign_response(id: int):
-        pass
 
     # TODO: implement
     def process_request(self):
@@ -154,8 +180,7 @@ class Request:
         """
         if self.packed_header[code_idx] == code_signing:
             try:
-                id = self.sign_client()
-                self.sign_response(id)
+                self.sign_client()
             except NameError:
                 print("NameError when trying to sign a new client user")
                 self.error_response()
@@ -175,7 +200,11 @@ class Request:
                 self.error_response()
 
         if self.packed_header[code_idx] == code_extract_msgs:
-            self.extract_msgs()
+            try:
+                self.extract_msgs()
+            except error:
+                print("Error. No waiting messages!")
+                self.error_response()
 
         if self.packed_header[code_idx] == code_send_msg:
             try:
